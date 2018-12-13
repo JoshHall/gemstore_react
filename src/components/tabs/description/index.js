9f4ef7c3ace98301e177e180843b1584bae7ec3e
Josh Hall <joshhall90@yahoo.com>
2018-12-13 12:07:44 -0500

    Made the tabs and fuctionality for tabs

---
+++ src/components/tabs/description/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Description extends Component {
+  render() {
+    return (
+      <div className="Description">
+        <h5>Description Tab</h5>
+      </div>
+    );
+  }
+}
+
+export default Description;
---
+++ src/components/tabs/reviews/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Reviews extends Component {
+  render() {
+    return (
+      <div className="Reviews">
+        <h5>Review Tab</h5>
+      </div>
+    );
+  }
+}
+
+export default Reviews;
